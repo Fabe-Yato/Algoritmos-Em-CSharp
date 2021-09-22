7d2e529855db302764ee4d48202f2289d656d9e3
Fabiano <Fabibinho2@gmail.com>
2021-09-22 00:37:11 -0300

    Adicionando mais alguns exercicios e trocando o nome do projeto

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e
 
 A fórmula para calcular a área de uma circunferência é area = π . raio². Considerando para este problema que π = 3.14159.
 Efetue o calculo da área.
+
+## Exercício 1003: Soma Simples
+
+Leia dois valores inteiros no caso variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. Imprima o resultado de SOMA.
+
+## Exercício 1004: Produto Simples
+
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação a variável PROD. Imprima a variável PROD.
---
+++ desafios.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace aula_4_facul
+{
+    class Program
+    {
+        public static void p1001(){
+            Console.Write("\n");
+             int A, B, X;
+             A = Convert.ToInt32(Console.ReadLine());
+
+             B = Convert.ToInt32(Console.ReadLine());
+             X = A + B;
+             Console.Write("\nX = {0}", X);
+             Console.Write("\nFIM");
+        }
+        public static void p1002(){
+            Console.Write("\n");
+            double pi = 3.14159;
+            double raio = Convert.ToDouble(Console.ReadLine());
+            double area = pi * (Math.Pow(raio,2));
+            //area.ToString(0.0000) tbm funciona para 4 casas decimais
+            Console.Write("\nA = {0}\n", Math.Round(area,4));
+            Console.Write("\nFIM"); 
+        }
+        public static void p1003(){
+            Console.Write("\n");
+            int a, b, SOMA;
+            a = Convert.ToInt32(Console.ReadLine());
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            SOMA = a + b;
+            Console.Write("\nSOMA = {0}", SOMA);
+            Console.Write("\nFIM");
+        }
+        public static void p1004(){
+            Console.Write("\n");
+            int a,b,PROD;
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            PROD = a * b;
+            Console.Write("\nPROD  = {0}", PROD);
+            Console.Write("\nFIM");
+        }
+        public static void p1010(){
+            Console.Write("1010");
+        }
+        static void Main(string[] args)
+        {
+            int prog;
+            do{
+            Console.Write("\n\t| 0 para sair |\nDigite o programa desejado: ");
+            prog = Convert.ToInt32(Console.ReadLine());
+
+            if(prog == 0){
+                Console.Write("\nSaindo...");
+                break;
+            }
+
+            switch(prog){
+                case 1001:
+                  Console.Write(prog);
+                  Program.p1001();
+                  break;
+
+                case 1002:
+                  Console.Write(prog);
+                  Program.p1002();
+                  break;
+
+                case 1003:
+                  Console.Write(prog);
+                  Program.p1003();
+                  break;
+                case 1004:
+                  Console.Write(prog);
+                  Program.p1004();
+                  break;
+
+                case 1010:
+                  Console.Write(prog);
+                  Program.p1010();
+                  break;
+
+                default:
+                  Console.Write("\n\tValor inválido\n");
+                  break;
+            }
+            }while(prog != 0);
+            
+        }
+    }
+}
